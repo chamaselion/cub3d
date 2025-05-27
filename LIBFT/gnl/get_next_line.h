a635947715ac6adb096ef46d62a1fccaaf545304
Balint Szikora <chamaselion@gmail.com>
2025-05-27 18:39:38 +0200

    Sharing ngl

---
+++ LIBFT/gnl/get_next_line.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 16:23:20 by bszikora          #+#    #+#             */
+/*   Updated: 2025/05/27 18:24:58 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>  // for open
+# include <stdio.h>  // for printf and perror
+# include <stdlib.h> // for malloc, realloc, and free
+# include <string.h> // for strlen and strchr
+# include <unistd.h> // for read and close
+
+typedef struct s_gnl
+{
+	char	*buffer[2];
+	char	*newline_pos;
+	ssize_t	bytes_read;
+	char	*line;
+	char	*tmp;
+	char	maybe;
+}			t_gnl;
+
+char		*ft_strchr(const char *s, int c);
+ssize_t		ft_strlen_gnl(const char *str);
+char		*get_next_line(int fd);
+void		*ft_memmove_gnl(void *dst, const void *src, ssize_t len);
+char		*ft_strjoin_gnl(char *s1, char *s2);
+
+#endif
---
+++ LIBFT/gnl/get_next_line_utils.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 16:27:00 by bszikora          #+#    #+#             */
+/*   Updated: 2025/05/27 18:24:53 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static void	ft_norminettebs(unsigned char *pdst, const unsigned char *psrc,
+		ssize_t len)
+{
+	ssize_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		pdst[i] = psrc[i];
+		i++;
+	}
+}
+
+void	*ft_memmove_gnl(void *dst, const void *src, ssize_t len)
+{
+	unsigned char	*pdst;
+	unsigned char	*psrc;
+	ssize_t			i;
+
+	pdst = (unsigned char *)dst;
+	psrc = (unsigned char *)src;
+	i = 0;
+	if (pdst == psrc)
+		return (dst);
+	if (pdst < psrc)
+	{
+		ft_norminettebs(pdst, psrc, len);
+	}
+	else
+	{
+		i = len;
+		while (i > 0)
+		{
+			pdst[i - 1] = psrc[i - 1];
+			i--;
+		}
+	}
+	return (dst);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	ch;
+	int		i;
+
+	ch = (char)c;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == ch)
+		{
+			return ((char *)&s[i]);
+		}
+		i++;
+	}
+	if (ch == '\0')
+	{
+		return ((char *)&s[i]);
+	}
+	return (0);
+}
+
+ssize_t	ft_strlen_gnl(const char *str)
+{
+	ssize_t	c;
+
+	c = 0;
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
+
+char	*ft_strjoin_gnl(char *s1, char *s2)
+{
+	char	*r;
+	ssize_t	c;
+	ssize_t	c1;
+
+	c = 0;
+	c1 = 0;
+	if (!s1 || !s2)
+		return (free(s1), NULL);
+	r = (char *)malloc(sizeof(char) * (ft_strlen_gnl(s1) + ft_strlen_gnl(s2) + 1));
+	if (!r)
+		return (free(s1), NULL);
+	while (s1[c] != '\0')
+	{
+		r[c] = s1[c];
+		c++;
+	}
+	while (s2[c1] != '\0')
+	{
+		r[c + c1] = s2[c1];
+		c1++;
+	}
+	r[c + c1] = '\0';
+	free(s1);
+	return (r);
+}
