44dcfc48f7437d3f6adcff9156aa347a23d1d7f2
Alima-T <okhapka.a@gmail.com>
2025-06-06 22:42:17 +0300

    Add function to check number of players on the map(only one player is allowed)

--- sources/checkers/checker.c
+++ sources/checkers/checker.c
@@ -6,7 +6,7 @@
 /*   By: alima <alima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 16:22:17 by alima             #+#    #+#             */
-/*   Updated: 2025/06/06 22:31:11 by alima            ###   ########.fr       */
+/*   Updated: 2025/06/06 22:39:06 by alima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,11 +127,38 @@ void normalize_spaces(t_game *game)
 	}
 }
 
+int check_player_number(t_game *game)
+{
+	int x;
+	int y;
+
+	x = 0;
+	y = 0;
+	int counter = 0;
+	while (y < game->height_map)
+	{
+		x = 0;
+		while (x < game->width_map)
+		{
+			if (game->map[y][x] == 'W' ||
+				game->map[y][x] == 'N' ||
+				game->map[y][x] == 'S' ||
+				game->map[y][x] == 'E')
+				counter++;
+			x++;
+		}
+		y++;
+	}
+	if (counter == 1)
+		return (0);
+	return (1);
+}
 void check_validity(t_game *game)
 {
 	int checker;
 
 	checker = check_walls(game);
+	checker += check_player_number(game);
 	if (checker != 0)
 	{
 		printf("Map validation failed\n");
@@ -139,6 +166,7 @@ void check_validity(t_game *game)
 		exit(EXIT_FAILURE);
 	}
 }
+
 void validate_map(t_game *game)
 {
 	normalize_spaces(game);  // Convert spaces to '0'
