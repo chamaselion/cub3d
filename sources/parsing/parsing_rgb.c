3053bbb0fd514e93255d5d3a90b58cb0e9e4e2a4
Alima-T <okhapka.a@gmail.com>
2025-06-24 22:56:28 +0200

    add function to check rgb number and refactor parse_rgb

--- sources/parsing/parsing_rgb.c
+++ sources/parsing/parsing_rgb.c
@@ -6,7 +6,7 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 11:01:31 by aokhapki          #+#    #+#             */
-/*   Updated: 2025/06/23 13:49:05 by aokhapki         ###   ########.fr       */
+/*   Updated: 2025/06/24 22:34:04 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 uint32_t	rgb_to_mlx_rgba(const int rgb[3])
 {
-    return ((uint32_t)rgb[0] << 24) | ((uint32_t)rgb[1] << 16) | ((uint32_t)rgb[2] << 8) | 255;
+    return ((uint32_t)rgb[0] << 24) | ((uint32_t)rgb[1] << 16) 
+		| ((uint32_t)rgb[2] << 8) | 255;
 }
 
 void	free_alloc(char **str)
@@ -45,33 +46,48 @@ int	is_pure_digit(const char *str)
 	return (1);
 }
 
-//TODO make shorter till 25 lines
+// This ensures that there are exactly 3 components.
+void	check_rgb_num(char **c)
+{
+	int	count = 0;
+
+	while (c[count])
+		count++;
+	if (count != 3)
+	{
+		printf("Error! RGB must have exactly 3 numbers\n");
+		free_alloc(c);
+		exit(EXIT_FAILURE);
+	}
+}
+
 int	parse_rgb(const char *ln, int rgb[3])
 {
-	int		i = 0;
+	int		i; 
+	int 	j;
 	char	**c;
+	char	*str;
 
+	i = 0;
 	while (ln[i] == ' ' || ft_isalpha(ln[i]))
 		i++;
 	c = ft_split(ln + i, ',');
+	check_rgb_num(c);
 	i = 0;
-	while (c[i] != NULL)
+	while (c[i])
 	{
-		if (!is_pure_digit(c[i]))
-		{
-			printf("Error! RGB values must be numeric only\n");
-			free_alloc(c);
-			exit(EXIT_FAILURE);
-		}
-		rgb[i] = ft_atoi(c[i]);
-		if (rgb[i] < 0 || rgb[i] > 255)
-		{
-			printf("Error! Invalid RGB numbers\n");
-			free_alloc(c);
-			exit(EXIT_FAILURE);
-		}
+		str = c[i];
+		while (*str == ' ' || *str == '\t') // skip leading spaces
+			str++;
+		j = 0;
+		while (str[j] && str[j] != '\n' && str[j] != '\t' && str[j] != ' ')
+			j++;
+		str[j] = '\0'; // cut off at first whitespace
+		if (!is_pure_digit(str) || (rgb[i] = ft_atoi(str)) < 0 || rgb[i] > 255)
+			err_free_exit_msg("Invalid RGB value", c);
 		i++;
 	}
 	free_alloc(c);
 	return (1);
 }
+
